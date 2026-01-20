a61ea08cf7f5ba073731a7735971cb956604ceed
Eliana Marmolejos <polancoeliana93@gmail.com>
2026-01-20 18:13:35 -0400

    Update CSS, JS and homepage template

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,10 @@
-// Futuro código JavaScript
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuButton = document.querySelector('.mobile-menu-button');
+    const navLinks = document.querySelector('#nav-links');
+
+    if (mobileMenuButton && navLinks) {
+        mobileMenuButton.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
+    }
+});
