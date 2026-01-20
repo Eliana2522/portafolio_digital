ca56167554cdbc265f090908cc4f1ce12e3e85b1
Eliana Marmolejos <polancoeliana93@gmail.com>
2026-01-20 16:32:52 -0400

    Fix ALLOWED_HOSTS for Render

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^==3ga49%y^_a6f519cl7
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = [
+    'portafolio-digital-qxh5.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
